40c4de57de42cf5c1d279e3879109bf35c383f69
Zohar Yzgiaev <43813108+zohar-yzgiaev@users.noreply.github.com>
2023-08-09 20:53:10 +0000

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const puppeteer = require('puppeteer');
 
+// Function to check if the page contains the text "more details" within a div with role="button"
+async function containsTextInRoleButton(page, text) {
+  const elements = await page.$$('div[role="button"]');
+  for (const element of elements) {
+    const textContent = await page.evaluate(el => el.textContent, element);
+    if (textContent.includes(text)) {
+      try {
+        await element.click();
+      }
+      catch(err){
+        console.log('err', err);
+      }
+    }
+  }
+  return false;
+}
 
 (async () => {
   try {
@@ -10,11 +26,11 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     
     // Navigate to the webpage you want to interact with
-    await page.goto('https://www.facebook.com/groups/458499457501175/'); // Replace 'https://example.com' with the actual URL
+    await page.goto('https://www.facebook.com/groups/458499457501175/');
     await page.waitForTimeout(2000);
 
     // Wait for the page to load completely
-    //await page.waitForNaviation({ waitUntil: 'domcontentloaded' });
+    // await page.waitForNaviation({ waitUntil: 'domcontentloaded' });
     await page.evaluate(() => {
       window.scrollBy(0, 500);
     });
@@ -35,24 +51,6 @@ const puppeteer = require('puppeteer');
     });
     await page.waitForTimeout(2000);
 
-    // Function to check if the page contains the text "more details" within a div with role="button"
-    async function containsTextInRoleButton(page, text) {
-      const elements = await page.$$('div[role="button"]');
-      for (const element of elements) {
-        const textContent = await page.evaluate(el => el.textContent, element);
-      
-        if (textContent.includes(text)) {
-          try{
-            await element.click();
-          }
-          catch(err){
-            console.log('err', err);
-          }
-        }
-      }
-      return false;
-    }
-
     // If the page contains "more details" within a div with role="button", click on it
     if (await containsTextInRoleButton(page, 'See more')) {
       console.log('Clicked on "more details" link within a div with role="button".');
@@ -62,15 +60,6 @@ const puppeteer = require('puppeteer');
 
     // Wait for a while to see the result of the click
     await page.waitForTimeout(3000); // Adjust the waiting time as needed
-
-
-
-
-
-
-
-
-
 
     const divSelector = 'div[role="feed"]'; // Selector for the div with role="feed"
     await page.waitForTimeout(2000);
@@ -95,73 +84,9 @@ const puppeteer = require('puppeteer');
     }, divSelector);
   
     console.log(nestedElements.children.map(child => child.text));
-  
-
-
-
-
-
-
-
-
-
     // Close the browser
     await browser.close();
   } catch (error) {
     console.error('Error:', error);
   }
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// async function scrapeNestedElements() {
-//   const browser = await puppeteer.launch({headless: false});
-//   const page = await browser.newPage();
-
-//   await page.goto('https://he-il.facebook.com/groups/295395253832427'); // Replace with the URL of the page containing the div you want to extract text from.
-
-//   const divSelector = 'div[role="feed"]'; // Selector for the div with role="feed"
-//   await page.waitForTimeout(15000);
-
-//   const nestedElements = await page.evaluate((selector) => {
-//     const feedDiv = document.querySelector(selector);
-
-//     if (!feedDiv) {
-//       return [];
-//     }
-
-//     const processElement = (element) => {
-//       const obj = {
-//         text: element.textContent.trim(),
-//         children: Array.from(element.children).map(processElement),
-//       };
-
-//       return obj;
-//     };
-
-//     return processElement(feedDiv);
-//   }, divSelector);
-
-//   console.log(nestedElements);
-
-//   await browser.close();
-// }
-
-// scrapeNestedElements();
